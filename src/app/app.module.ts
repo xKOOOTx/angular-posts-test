f190b600e7c87e69d5505309e37a1494d60e78b2
Vladimir Sadovnokov <fayst_@bk.ru>
2021-12-21 14:29:02 +0300

    finished todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import {NgModule} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { PostsListComponent } from './posts/pages/posts-list/posts-list.component';
-import { PostsTableComponent } from './posts/pages/posts-table/posts-table.component';
-import { AddPostComponent } from './pages/add-post/add-post.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from "@angular/material/paginator";
-import {ReactiveFormsModule} from "@angular/forms";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {PostsListComponent} from './posts/pages/posts-list/posts-list.component';
+import {PostsTableComponent} from './posts/pages/posts-table/posts-table.component';
+import {AddPostComponent} from './pages/add-post/add-post.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import { TodosComponent } from './pages/todos/todos.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {TodosComponent} from './pages/todos/todos.component';
+import {MatSortModule} from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -27,19 +28,20 @@ import { TodosComponent } from './pages/todos/todos.component';
     AddPostComponent,
     TodosComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        ReactiveFormsModule,
-        MatCardModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatSortModule
+  ],
   providers: [
   ],
   bootstrap: [AppComponent]
--- src/app/pages/todos/todos.component.ts
+++ src/app/pages/todos/todos.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import {TodosService} from '../../services/todos.service';
 import {Subscription} from 'rxjs';
+import {MatSort, Sort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {coerceNumberProperty} from '@angular/cdk/coercion';
 
 
 @Component({
@@ -8,22 +14,53 @@ import {Subscription} from 'rxjs';
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, AfterViewInit {
 
-  data:any;
   sub!: Subscription;
+  displayedColumns: string[] = ['number', 'title', 'completed', 'delete'];
+  dataSource = new MatTableDataSource();
 
-  constructor(private toDosService: TodosService) { }
+  constructor(
+    private toDosService: TodosService,
+    private _liveAnnouncer: LiveAnnouncer,
+    private fb: FormBuilder
+  ) { }
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngOnInit(): void {
-    this.getTodos()
+    this.getTodos();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   getTodos() {
     this.toDosService.getTodos()
       .subscribe(res => {
-        console.log(res)
-      })
+        this.dataSource.data = res;
+        console.log(res);
+      });
   }
 
+  deleteTodo(element:any) {
+    let idx = this.dataSource.filteredData.indexOf(element);
+    this.dataSource.filteredData.splice(idx, 1);
+    this.dataSource.data = this.dataSource.filteredData;
+  }
+
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
