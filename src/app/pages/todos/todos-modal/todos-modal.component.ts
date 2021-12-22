05aeb02ee25347937f0ebec32418bac9c627b692
Vladimir Sadovnokov <fayst_@bk.ru>
2021-12-22 15:25:42 +0300

    finished add todo popup

--- src/app/pages/todos/todos-modal/todos-modal.component.ts
+++ src/app/pages/todos/todos-modal/todos-modal.component.ts
@@ -1,6 +1,10 @@
-import {Component, OnInit, Inject, Output, EventEmitter} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
-
+import {Component, Inject, OnInit} from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {DialogData} from '../pages/todos.component';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {ThemePalette} from '@angular/material/core';
+import {TodosService} from '../../../services/todos.service';
 
 @Component({
   selector: 'app-todos-modal',
@@ -9,29 +13,47 @@ import {FormBuilder, Validators} from '@angular/forms';
 })
 export class TodosModalComponent implements OnInit {
 
-  @Output()
-  change: EventEmitter<any> = new EventEmitter<any>();
-
-  isModalOpen:boolean = true;
+  color: ThemePalette = "primary";
+  required: boolean = true;
   todosModalForm: any;
+  isTaskCompletedBool: boolean = false;
 
   constructor(
-    private fb: FormBuilder
-    ) { }
+    private toDosService: TodosService,
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<TodosModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    this.initForm()
+    this.modalFormInit()
   }
 
-  initForm() {
-    this.todosModalForm = this.fb.group({
-      id: [''],
-      title: '',
-      completed: ['']
+  modalFormInit() {
+    this.todosModalForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+      isTaskCompleted: new FormControl('')
     })
   }
 
-  closeModal() {
-    this.change.emit()
+  onNoClick(): void {
+    this.dialogRef.close();
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.todosModalForm.reset();
+  }
+
+  sendTodos() {
+    // this.toDosService.sendTodos(this.todosModalForm.value)
+    console.log(this.todosModalForm.value)
+    this.resetForm()
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
   }
 }
